7d235991edd146afea55d3afb59a233dc5ba44e1
Ovesh101 <Oveshmulla786@gmail.com>
2024-05-21 19:14:33 +0530

    added show list componenets

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -2,84 +2,84 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-import {AiOutlineDelete, AiOutlineEdit} from 'react-icons/ai'
-import {BsInfoCircle} from 'react-icons/bs'
-import {MdOutlineAddBox , MdOutlineDelete} from 'react-icons/md'
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 import Spinner from '../components/Spinner'
 
 
 const Home = () => {
-    const [books , setBooks] = useState([])
-    const [loading , setLoading] = useState(false);
+    const [books, setBooks] = useState([])
+    const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         setLoading(true)
-        axios.get("http://localhost:3000/books").then((response)=>{
+        axios.get("http://localhost:3000/books").then((response) => {
             setBooks(response.data.data);
             console.log(response.data.data);
             setLoading(false)
-        }).catch((err)=>console.log(err))
-    } , [])
-    
-  return (
-   <div className='p-4'>
-    <div className='flex justify-between items-center mb-4'>
-        <h1 className='text-3xl my-8'>Book List</h1>
+        }).catch((err) => console.log(err))
+    }, [])
 
-        <Link to="/books/createBook">
-            <MdOutlineAddBox className='text-sky-800 text-4xl'  />
-        </Link>
-    </div>
-    {
-    loading ? ( <Spinner /> ) : (
-        <table className='w-full border-seperate border-spacing-2 '>
-            <thead>
-                <tr>
-                    <th className='border border-slate-600 rounded-md '>No</th>
-                    <th className='border border-slate-600 rounded-md  '>Title</th>
-                    <th className='border border-slate-600 rounded-md max-md:hidden '>Author</th>
-                    <th className='border border-slate-600 rounded-md max-md:hidden '>Publish Year</th>
-                    <th className='border border-slate-600 rounded-md '>Operation</th>
-                </tr>
+    return (
+        <div className='p-4'>
+            <div className='flex justify-between items-center mb-4'>
+                <h1 className='text-3xl my-8'>Book List</h1>
 
-            </thead>
+                <Link to="/books/createBook">
+                    <MdOutlineAddBox className='text-sky-800 text-4xl' />
+                </Link>
+            </div>
+            {
+                loading ? (<Spinner />) : (
+                    <table className='w-full border-seperate border-spacing-2 '>
+                        <thead>
+                            <tr>
+                                <th className='border border-slate-600 rounded-md '>No</th>
+                                <th className='border border-slate-600 rounded-md  '>Title</th>
+                                <th className='border border-slate-600 rounded-md max-md:hidden '>Author</th>
+                                <th className='border border-slate-600 rounded-md max-md:hidden '>Publish Year</th>
+                                <th className='border border-slate-600 rounded-md '>Operation</th>
+                            </tr>
 
-            <tbody>
-                {books.map((book , index)=>{
-                 
-                 return <tr key={book._id} className='h-8'>
-                        <td className='border border-slate-700 rounded-md text-center'>{index+1}</td>
-                        <td className='border border-slate-700 rounded-md text-center'>{book.title}</td>
-                        <td className='border border-slate-700 rounded-md max-md:hidden text-center'>{book.author}</td>
-                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.publishedYear}</td>
-                        <td className='border border-slate-700 rounded-md text-center'>
-                            <div className='flex justify-center gap-x-4'>
-                                <Link to={`/books/details/${book._id}`}>
-                                    <BsInfoCircle className='text-2xl text-green-600' />
+                        </thead>
 
-                                </Link>
-                                <Link to={`/books/editBook/${book._id}`}>
-                                    <AiOutlineEdit className='text-2xl text-yellow-600' />
-                                    
-                                </Link>
-                                <Link to={`/books/deleteBook/${book._id}`}>
-                                    <AiOutlineDelete className='text-2xl text-red-600' />
-                                    
-                                </Link>
+                        <tbody>
+                            {books.map((book, index) => {
 
-                            </div>
-                        </td>
-                    </tr>
+                                return <tr key={book._id} className='h-8'>
+                                    <td className='border border-slate-700 rounded-md text-center'>{index + 1}</td>
+                                    <td className='border border-slate-700 rounded-md text-center'>{book.title}</td>
+                                    <td className='border border-slate-700 rounded-md max-md:hidden text-center'>{book.author}</td>
+                                    <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.publishedYear}</td>
+                                    <td className='border border-slate-700 rounded-md text-center'>
+                                        <div className='flex justify-center gap-x-4'>
+                                            <Link to={`/books/details/${book._id}`}>
+                                                <BsInfoCircle className='text-2xl text-green-600' />
 
-                })}
-            </tbody>
+                                            </Link>
+                                            <Link to={`/books/editBook/${book._id}`}>
+                                                <AiOutlineEdit className='text-2xl text-yellow-600' />
 
-        </table>
+                                            </Link>
+                                            <Link to={`/books/deleteBook/${book._id}`}>
+                                                <AiOutlineDelete className='text-2xl text-red-600' />
+
+                                            </Link>
+
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            })}
+                        </tbody>
+
+                    </table>
+                )
+
+            }
+        </div>
     )
-
-    }
-   </div>
-  )
 }
 
 export default Home
--- fronted/src/pages/ShowList.jsx
+++ fronted/src/pages/ShowList.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import backButton from '../components/backButton'
+import Spinner from '../components/Spinner'
 
 const ShowList = () => {
+  const [loading, setLoading] = useState(false);
+  const [book, setBook] = useState({});
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`http://localhost:3000/books/${id}`).then((response) => {
+      setBook(response.data.data);
+      setLoading(false);
+      console.log(response.data.data);
+    }).catch((err) => {
+      console.log(err.message);
+      setLoading(false)
+    })
+
+  
+  } , [])
   return (
-    <div>ShowList</div>
+    <div className='p-4'>
+      <backButton />
+      <h1 className='text-3xl my-4'> Show Book</h1>
+      {
+      loading ? (<Spinner />) :
+      (
+        <div className='felx flex-col border-2 border-sky-100 rounded-xl w-fit p-4'>
+          <div className='my-4 '>
+            <span className='text-xl mr-4 text-gray-500' >ID</span>
+            <span>{book._id}</span>
+          </div>
+          <div className='my-4 '>
+            <span className='text-xl mr-4 text-gray-500' >Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className='my-4 '>
+            <span className='text-xl mr-4 text-gray-500' >Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className='my-4 '>
+            <span className='text-xl mr-4 text-gray-500' >Publish Year</span>
+            <span>{book.publishedYear}</span>
+          </div>
+        </div>
+      )
+      }
+    </div>
+
+    
   )
 }
 
